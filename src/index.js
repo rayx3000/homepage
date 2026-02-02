84bd328d8a10ab6dc147b41a52ff0d67b20b1d59
rayx3000 <candole2020@gmail.com>
2026-02-02 15:41:38 +0800

    add projects section

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import './styles/general.css';
 import './styles/header.css';
 import './styles/main.css';
 
-console.log("Hi baby!");
+import { projectData } from './scripts/data';
+
+let projects = '';
+projectData.forEach(project => {
+    projects += `
+    <div class="project-list" id="${project.projectId}">
+        <img src="${project.projectImg}" alt="${project.projectName} Image" class="project-image"/>
+        <h3 class="project-name">${project.projectName}</h3>
+        <p class="project-desc">${project.projectDesc}</p>
+    </div>
+    `;
+});
+
+document.querySelector('.projects-list').innerHTML = projects;
+
---
+++ src/scripts/data.js
@@ -0,0 +1,29 @@
+export let projectData = [
+    {
+        projectName: "Admin Dashboard",
+        projectDesc: `This admin dashboard is created to strengthen my experience in CSS layout using Flexbox and Grid.
+                    this project helps me develop my skills to developed more technqiues in web design and frontend 
+                    development. using CSS Flexbox and Grid made the layout more easier to manage and organized the 
+                    content of HTML elements.`,
+        projectImg: "./src/assets/admin-dashboard.png",
+        projectId: "admin-dashboard"
+    },
+    {
+        projectName: "Search Engine",
+        projectDesc: `This search engine project is built with HTML, CSS, and JavaScript to demonstrate my frontend 
+                    development skills. It features a responsive design and interactive elements to enhance user 
+                    experience. With my own creativity this search engine showcases my ability to create functional 
+                    and visually appealing web applications.`,
+        projectImg: "./src/assets/search_engine.png",
+        projectId: "eyy-template"
+    },
+    {
+        projectName: "Weather App",
+        projectDesc: `A weather application built with HTML, CSS, and JavaScript that fetches and displays real-time 
+                    weather data using API, async and DOM manipulation. It helps understand how asynchronous JavaScript
+                    works and how to interact with external APIs to retrieve data. This project showcases my ability
+                    to create dynamic web applications that provide real-time information to users.`,
+        projectImg: "./src/assets/rayx_weather.png",
+        projectId: "weather-app"
+    }
+];
