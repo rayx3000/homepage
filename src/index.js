7d60f1a910f4e8d9841ccc1848fee1a3072b81d0
rayx3000 <candole2020@gmail.com>
2026-02-02 16:44:04 +0800

    generate html in js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import { projectData } from './scripts/data';
 
 let projects = '';
 projectData.forEach(project => {
-    projects += `
-    <div class="project-list" id="${project.projectId}">
-        <img src="${project.projectImg}" alt="${project.projectName} Image" class="project-image"/>
-        <h3 class="project-name">${project.projectName}</h3>
-        <p class="project-desc">${project.projectDesc}</p>
-    </div>
-    `;
+    projects += ` <div class="project" id="${project.projectId}">
+                    <div class="project-image">
+                        <img src="${project.projectImg}" alt="${project.projectName} Image">
+                    </div>
+                    <div class="project-info">
+                        <h3>${project.projectName}</h3>
+                        <p>${project.projectDesc}</p>
+                    </div>
+                    <div class="project-links">
+                        <a href="https://rayx3000.github.io/${project.projectId}/" target="_blank"><span><i class="fa-solid fa-globe"></i> <span>Live View</span></span></a>
+                        <a href="https://github.com/rayx3000/${project.projectId}" target="_blank"><span><i class="fa-solid fa-code"></i> <span>Source Code</span></span></a>
+                    </div>
+                    </div>`;
+                    ;
 });
 
-document.querySelector('.projects-list').innerHTML = projects;
-
+document.querySelector('.project-list').innerHTML = projects;
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,3 +1,13 @@
+import adminDashboardImage from '../assets/admin_dashboard.png';
+import searchEngineImage from '../assets/search_engine.png';
+import weatherAppImage from '../assets/rayx_weather.png';
+import signUpFormImage from '../assets/sign_up_form.png';
+import rockPaperScissorsImage from '../assets/rock_paper_scissors.png';
+import restaurantAppImage from '../assets/rayx_resto.png';
+import digitalClockImage from '../assets/digital_clock.png';
+import todoListImage from '../assets/to_do_list.png';
+
+
 export let projectData = [
     {
         projectName: "Admin Dashboard",
@@ -5,7 +15,7 @@ export let projectData = [
                     this project helps me develop my skills to developed more technqiues in web design and frontend 
                     development. using CSS Flexbox and Grid made the layout more easier to manage and organized the 
                     content of HTML elements.`,
-        projectImg: "./src/assets/admin-dashboard.png",
+        projectImg: adminDashboardImage,
         projectId: "admin-dashboard"
     },
     {
@@ -14,7 +24,7 @@ export let projectData = [
                     development skills. It features a responsive design and interactive elements to enhance user 
                     experience. With my own creativity this search engine showcases my ability to create functional 
                     and visually appealing web applications.`,
-        projectImg: "./src/assets/search_engine.png",
+        projectImg: searchEngineImage,
         projectId: "eyy-template"
     },
     {
@@ -23,7 +33,47 @@ export let projectData = [
                     weather data using API, async and DOM manipulation. It helps understand how asynchronous JavaScript
                     works and how to interact with external APIs to retrieve data. This project showcases my ability
                     to create dynamic web applications that provide real-time information to users.`,
-        projectImg: "./src/assets/rayx_weather.png",
+        projectImg: weatherAppImage,
         projectId: "weather-app"
+    },
+    {
+        projectName: "Sign Up Form",
+        projectDesc: `A sign up form built with HTML, CSS, and JavaScript that demonstrates form validation and user input handling. 
+                    It helps understand how to create interactive forms and validate user data on the frontend. This project showcases my ability
+                    to build functional web forms with proper validation and user experience considerations.`,
+        projectImg: signUpFormImage,
+        projectId: "sign-up-form"
+    },
+    {
+        projectName: "Rock Paper Scissors",
+        projectDesc: `A Rock Paper Scissors game built with HTML, CSS, and JavaScript that demonstrates game logic and user interaction. 
+                    It helps understand how to implement game mechanics and handle user input in a web application. This project showcases my ability
+                    to build interactive games with proper game state management.`,
+        projectImg: rockPaperScissorsImage,
+        projectId: "rock-paper-scissors"
+    },
+    {
+        projectName: "Restaurant",
+        projectDesc: `A restaurant application built with HTML, CSS, and JavaScript that demonstrates a responsive design and interactive user interface. 
+                    It helps understand how to build a complete web application with multiple pages and navigation. This project showcases my ability
+                    to create fully functional web applications with a focus on user experience. and It introduces webpack for module bundling and asset management.`,
+        projectImg: restaurantAppImage,
+        projectId: "restaurant-app"
+    },
+    {
+        projectName: "Digital Clock",
+        projectDesc: `A digital clock application built with HTML, CSS, and JavaScript that demonstrates real-time updates and DOM manipulation. 
+                    It helps understand how to update the UI dynamically and manage time-related logic in a web application. This project showcases my ability
+                    to build interactive applications that respond to real-time events.`,
+        projectImg: digitalClockImage,
+        projectId: "digital-clock"
+    },
+    {
+        projectName: "To-Do List",
+        projectDesc: `A to-do list application built with HTML, CSS, and JavaScript that demonstrates task management and user interaction. 
+                    It helps understand how to create and manage dynamic lists in a web application. This project showcases my ability
+                    to build interactive applications with proper state management. It helps me to learn complex DOM manipulation and event handling in JavaScript.`,
+        projectImg: todoListImage,
+        projectId: "todo-list"
     }
 ];
